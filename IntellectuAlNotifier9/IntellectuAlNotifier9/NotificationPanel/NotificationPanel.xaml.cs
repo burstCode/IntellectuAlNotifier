3b9afe284a8561553cc25082abc94eb2d0af5361
LuchokSs <sdomahnev@gmail.com>
2025-02-19 11:46:36 +0300

    Удалена папка со старым проектом.

---
+++ IntellectuAlNotifier9/IntellectuAlNotifier9/NotificationPanel/NotificationPanel.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntellectuAlNotifierDesktop
+{
+	/// <summary>
+	/// Логика взаимодействия для NotificationPanel.xaml
+	/// </summary>
+	public partial class NotificationPanel : UserControl
+	{
+		public NotificationPanel()
+		{
+			InitializeComponent();
+		}
+
+		private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+		{
+			(App.Current.MainWindow as Window).WindowState = WindowState.Minimized;
+		}
+
+		private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+		{
+			if ((App.Current.MainWindow as Window).WindowState == WindowState.Maximized)
+			{
+				(App.Current.MainWindow as Window).WindowState = WindowState.Normal;
+			}
+			else
+			{
+				(App.Current.MainWindow as Window).WindowState = WindowState.Maximized;
+			}
+			RefreshMaximizeRestoreButton();
+		}
+
+		private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+		{
+			(App.Current.MainWindow as Window).Close();
+		}
+
+		private void RefreshMaximizeRestoreButton()
+		{
+			if ((App.Current.MainWindow as Window).WindowState == WindowState.Maximized)
+			{
+				this.maximizeButton.Visibility = Visibility.Collapsed;
+				this.restoreButton.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				this.maximizeButton.Visibility = Visibility.Visible;
+				this.restoreButton.Visibility = Visibility.Collapsed;
+			}
+		}
+
+		private void Window_StateChanged(object sender, EventArgs e)
+		{
+			this.RefreshMaximizeRestoreButton();
+		}
+	}
+}
---
+++ IntellectuAlNotifier9/IntellectuAlNotifier9/ViewLayout.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace IntellectuAlNotifierDesktop
+{
+    public class ViewboxPanel : Panel
+    {
+        private double scale;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            double height = 0;
+            Size unlimitedSize = new Size(double.PositiveInfinity, double.PositiveInfinity);
+            foreach (UIElement child in Children)
+            {
+                child.Measure(unlimitedSize);
+                height += child.DesiredSize.Height;
+            }
+            scale = availableSize.Height / height;
+
+            foreach (UIElement child in Children)
+            {
+                unlimitedSize.Width = availableSize.Width / scale;
+                child.Measure(unlimitedSize);
+            }
+
+            return availableSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            Transform scaleTransform = new ScaleTransform(scale, scale);
+            double height = 0;
+            foreach (UIElement child in Children)
+            {
+                child.RenderTransform = scaleTransform;
+                child.Arrange(new Rect(new Point(0, scale * height), new Size(finalSize.Width / scale, child.DesiredSize.Height)));
+                height += child.DesiredSize.Height;
+            }
+
+            return finalSize;
+        }
+    }
+}
